f108e92f94fd6a40950f549acc525ce75fba082d
cassi2shoes <97320334+cassi2shoes@users.noreply.github.com>
2025-10-25 01:49:23 +0000

    i may be mistaken

---
+++ Game.Java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class main{
+    public static void(String[] args)
+    {
+        String answer = wordPick;
+        system.out.println(answer);
+
+    }
+
+    public static string wordPick ()
+    {
+        int x = (int) (Math.random()*(5-1+1)) +1;
+        if (x == 1)
+        {
+            return "Halloween";
+        }
+        else if (x==2)
+        {
+            return"Skeleton";
+        }
+        else if (x==3)
+        {
+            return "Pumpkin";
+        }
+        else if (x==4)
+        {
+            return "Candy";
+        }
+        else
+        {
+            return "Spooky";
+        }
+}
+}
