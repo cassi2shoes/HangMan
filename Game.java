2199e3b1a5e379f680dbe8673a09a341659d93c8
cassi2shoes <97320334+cassi2shoes@users.noreply.github.com>
2025-10-25 03:15:53 +0000

    litterally most of the project. guessing no work

--- Game.java
+++ Game.java
@@ -1,14 +1,46 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class main{
-    public static void(String[] args)
+//FIX THE GUESSING try the method again. DONT FORGET TO MAKE "scanner.nextLine()" LOWERCASE
+
+public class Game{
+    
+    public static void main(String[] args)
     {
-        String answer = wordPick;
-        system.out.println(answer);
-
+        Scanner scanner = new Scanner(System.in);    
+        String answer = wordPick();
+        System.out.println("Welcome to Halloween Themed Hangman!");
+        System.out.println("--|");
+        System.out.println("|");
+        System.out.println("|");
+        System.out.println("___");
+        System.out.println("");
+        System.out.println(spellOut(answer));
+        int points = 0;
+        int lose = 6;
+        String results = "";
+        while (points < lose || results.equals(answer))
+        {
+            System.out.println("Please type your guess!");
+            String newGuess = scanner.nextLine();
+            boolean replace = guess(answer, newGuess);
+            if(replace = true)
+            {
+                for(int i = 0; i<answer.length();i++)
+                {
+                    if(newGuess.equals(answer.substring(i,i+1)))
+                    {
+                        results+=newGuess;
+                    }
+                    else
+                    {
+                        results+= "- ";
+                    }
+                }
+            }
+        }
+    
     }
-
-    public static string wordPick ()
+    public static String wordPick()
     {
         int x = (int) (Math.random()*(5-1+1)) +1;
         if (x == 1)
@@ -31,5 +63,38 @@ public class main{
         {
             return "Spooky";
         }
-}
+    }
+    public static String spellOut(String word)
+    {
+        String x = "";
+        for (int i = 0; i <= word.length()-1; i++)
+        {
+            x += "- ";
+        }
+        return x;
+    }
+    public static boolean guess(String check, String g)
+    {
+        boolean correct = false;
+            if (g.length()>1)
+            {
+                System.out.println("please enter a guess that is 1 letter");
+            }
+            else if (check.indexOf(g)== -1)
+            {
+                System.out.println("That's incorrect!");
+            }
+            else
+            {
+                for(int i = 0; i<check.length();i++)
+                {
+                    if(g.equals(check.substring(i,i+1)))
+                    {
+                        correct = true;
+                    }
+                }
+
+            }
+            return correct;
+        }   
 }
