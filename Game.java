d0af948a6b3ff88a3544a28d31264423d69e4877
cassi2shoes <97320334+cassi2shoes@users.noreply.github.com>
2025-10-27 01:53:59 +0000

    ITS DONEEEE

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.util.Arrays;
 //FIX THE GUESSING try the method again. DONT FORGET TO MAKE "scanner.nextLine()" LOWERCASE
 
 public class Game{
@@ -9,45 +10,66 @@ public class Game{
         Scanner scanner = new Scanner(System.in);    
         String answer = wordPick();
         System.out.println("Welcome to Halloween Themed Hangman!");
-        System.out.println("--|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("___");
+        System.out.println(ascii(0));
         System.out.println("");
         System.out.println(spellOut(answer));
         char[] key = toCheck(answer);
+        char[] play = buildWord(answer);
         int points = 0;
         int lose = 6;
-        String results = "";
+        String incorrectGuesses ="These are the letters you have guessed incorrectly: ";
         while (points < lose)
         {
-            System.out.println("Please type your guess!");
-            String newGuess = scanner.nextLine();
-            boolean replace = guess(answer, newGuess);
-            if(replace = true)
+            System.out.println("Please enter a guess!");
+            String user = scanner.nextLine();
+            String lowerUser = user.toLowerCase();
+            String compariasonString = String.valueOf(play);
+            int trueOrFalse = answer.indexOf(lowerUser);
+            boolean a = trueOrFalse == -1;
+            if (user.length() > 1)
             {
-                char[] set = new char [answer.length()-1];
-                for(int i = 0; i<answer.length();i++)
+                System.out.println("please make your guess one(1) character long.");
+            }
+            else if (a == true)
+            {
+                System.out.println("that guess is incorrect");
+                points++;
+                incorrectGuesses+=" " + lowerUser;
+                System.out.println("you have " + (lose-points) + " incorrect guesses left before you lose");
+                System.out.println(incorrectGuesses);
+                System.out.println(ascii(points));
+                System.out.println(play);
+            }
+            else if (location(compariasonString,lowerUser) != -1)
+            {
+                System.out.println("You've already guessed this letter correctly");
+            }
+            else
+            {
+                char realGuess = lowerUser.charAt(0);
+                int place = location(answer,lowerUser);
+                play[place] = realGuess;
+                System.out.println(incorrectGuesses);
+                System.out.println(ascii(points));
+                System.out.println(play);
+                if(Arrays.equals(play,key))
                 {
-                    if(newGuess.equals(answer.substring(i,i+1)))
-                    {
-                        current+=newGuess;
-                        
-                    }
-                    else
-                    {
-                        current+= "- ";
-
-                    }
+                    points+=50;
                 }
-                System.out.println(current);
             }
-            if (results.equals(answer))
-            {
-                System.out.println("You found the word!");
-                System.out.println(results);
-                break;
-            }
+        }
+        if(points>49)
+        {
+            System.out.println("You Win!");
+        }
+        else
+        {
+            System.out.println("you lost :(");
+            System.out.println("--|");
+            System.out.println("| o");
+            System.out.println("|/|\\");
+            System.out.println("|/ \\");
+            System.out.println("____");
         }
     
     }
@@ -56,28 +78,38 @@ public class Game{
         int x = (int) (Math.random()*(5-1+1)) +1;
         if (x == 1)
         {
-            return "Halloween";
+            return "scary";
         }
         else if (x==2)
         {
-            return"Skeleton";
+            return"costume";
         }
         else if (x==3)
         {
-            return "Pumpkin";
+            return "ghost";
         }
         else if (x==4)
         {
-            return "Candy";
+            return "candy";
         }
         else
         {
-            return "Spooky";
+            return "monster";
         }
     }
     public static char[] toCheck(String a)
     {
         char[] b = a.toCharArray();
+        return b;
+    }
+    public static char[] buildWord(String a)
+    {
+        int x = a.length();
+        char[] b = new char[x];
+        for (int i = 0; i<a.length(); i++)
+        {
+            b[i]='-';
+        }
         return b;
     }
     public static String spellOut(String word)
@@ -89,28 +121,13 @@ public class Game{
         }
         return x;
     }
-    public static boolean guess(String check, String g)
+    public static int location(String a , String letter)
     {
-        boolean correct = false;
-            if (g.length()>1)
-            {
-                System.out.println("please enter a guess that is 1 letter");
-            }
-            else if (check.indexOf(g)== -1)
-            {
-                System.out.println("That's incorrect!");
-            }
-            else
-            {
-                for(int i = 0; i<check.length();i++)
-                {
-                    if(g.equals(check.substring(i,i+1)))
-                    {
-                        correct = true;
-                    }
-                }
-
-            }
-            return correct;
-        }   
+        return a.indexOf(letter);
+    }
+    public static String ascii(int point)
+    {
+        String[] asciiStorage = {"--|\n|  \n|   \n|   \n____","--|\n| o \n|   \n|   \n____","--|\n| o\n| | \n|  \n___","--|\n| o\n|/| \n|  \n___","--|\n| o\n|/|\\ \n|  \n___","--|\n| o\n|/|\\ \n|/ \n___","--|\n| o\n|/|\\ \n|/ \\ \n___"};
+        return asciiStorage[point];
+    }
 }
